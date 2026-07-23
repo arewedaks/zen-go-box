d25cdd2aa2a3a3516f854f06cc8923ca681fcea2
arewedaks <tempeduai9@gmail.com>
2026-07-23 19:54:19 +0700

    Update shell scripts to use zengobox paths

--- shell/action.sh
+++ shell/action.sh
@@ -1,4 +1,4 @@
 #!/system/bin/sh
 
-# Panggil command zennode toggle sebagai superuser
-su -c "/data/adb/zennode/bin/zennode toggle"
+# Panggil command zengobox toggle sebagai superuser
+su -c "/data/adb/zengobox/bin/zengobox toggle"
--- shell/uninstall.sh
+++ shell/uninstall.sh
@@ -1,23 +1,23 @@
 #!/system/bin/sh
 
-zennode_data_dir="/data/adb/zennode"
+zengobox_data_dir="/data/adb/zengobox"
 
 # Hentikan proxy service & bersihkan netfilter sebelum uninstall
-if [ -f "${zennode_data_dir}/bin/zennode" ]; then
-    "${zennode_data_dir}/bin/zennode" stop >/dev/null 2>&1
+if [ -f "${zengobox_data_dir}/bin/zengobox" ]; then
+    "${zengobox_data_dir}/bin/zengobox" stop >/dev/null 2>&1
 fi
 
 rm_data() {
-  if [ -d "${zennode_data_dir}" ]; then
-    rm -rf "${zennode_data_dir}"
+  if [ -d "${zengobox_data_dir}" ]; then
+    rm -rf "${zengobox_data_dir}"
   fi
 
-  if [ -f "/data/adb/ksu/service.d/zennode_service.sh" ]; then
-    rm -rf "/data/adb/ksu/service.d/zennode_service.sh"
+  if [ -f "/data/adb/ksu/service.d/zengobox_service.sh" ]; then
+    rm -rf "/data/adb/ksu/service.d/zengobox_service.sh"
   fi
 
-  if [ -f "/data/adb/service.d/zennode_service.sh" ]; then
-    rm -rf "/data/adb/service.d/zennode_service.sh"
+  if [ -f "/data/adb/service.d/zengobox_service.sh" ]; then
+    rm -rf "/data/adb/service.d/zengobox_service.sh"
   fi
 }
 
